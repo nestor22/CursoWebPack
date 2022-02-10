db78f34f7cf5b69e3839112fa7e6a0898f8da394
ngonzalez <nestor.ge222@gmail.com>
2022-02-09 20:30:24 -0600

    se agrega configuracion de archivo.env

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,4 +1,4 @@
-const API = 'https://randomuser.me/api/';
+const API = process.env.API;
 
 const getData = async (id) => {
   const apiURl = id ? `${API}${id}` : API;
@@ -6,7 +6,7 @@ const getData = async (id) => {
     const response = await fetch(apiURl);
     const data = await response.json();
     return data.results[0];
-  } catch (error) {
+  } catch (error) { 
     console.log('Fetch Error', error);
   };
 };
